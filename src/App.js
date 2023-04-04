876ea3e94ddfba3a5c7dbdec654d0486b9de0bcf
shijie09 <jiesj09@gmail.com>
2023-04-04 16:01:22 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useState } from "react"
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header"
-import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
 import FeedbackForm from "./components/FeedbackForm"
